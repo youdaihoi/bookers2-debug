1f5606aa0e78e795a9e194bd00bc0a6067ffb547
youdaihoi <yohdai.camel@gmail.com>
2021-12-28 16:38:25 +0000

    コメント２

--- app/controllers/bookcomments_controller.rb
+++ app/controllers/bookcomments_controller.rb
@@ -1,5 +1,5 @@
 class BookcommentsController < ApplicationController
-  before_action :ensure_correct_user, only: [:destroy]
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.bookcomments.new(bookcomment_params)
