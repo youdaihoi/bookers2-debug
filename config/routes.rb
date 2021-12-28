ab4a5923803bc30b2740efbd1235da672ffb47e2
youdaihoi <yohdai.camel@gmail.com>
2021-12-28 13:30:04 +0000

    いいね

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
